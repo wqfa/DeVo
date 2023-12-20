01bb6e0cfb5f9386aed27ae0fd221240b9ae10d7
XF <118357460+wqfa@users.noreply.github.com>
2023-12-20 19:40:41 +0300

    Create Tepthon.py

---
+++ Tepthon.py
@@ -0,0 +1,526 @@
+import telethon
+from telethon import events
+from config import *
+import os
+import logging
+import asyncio
+import time
+from telethon.tl import functions, types
+from telethon.tl.functions.messages import ImportChatInviteRequest as Get
+from telethon.utils import get_display_name
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.errors import FloodWaitError
+from telethon import TelegramClient, events
+from collections import deque
+from telethon import functions
+from telethon.errors.rpcerrorlist import (
+    UserAlreadyParticipantError,
+    UserNotMutualContactError,
+    UserPrivacyRestrictedError,
+)
+from telethon.tl.functions.channels import InviteToChannelRequest
+from telethon.tl.types import InputPeerUser
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl import functions
+from hijri_converter import Gregorian
+from telethon.tl.functions.channels import LeaveChannelRequest
+import base64
+import datetime
+from payment import *
+from help import *
+from checktele import *
+from telethon.tl.functions.messages import GetHistoryRequest
+from telethon.tl.functions.messages import ImportChatInviteRequest
+import requests
+# -
+
+Tepthon.start()
+c = requests.session()
+bot_username = '@zmmbot'
+bot_usernamee = '@A_MAN9300BOT'
+
+y = datetime.datetime.now().year
+m = datetime.datetime.now().month
+dayy = datetime.datetime.now().day
+day = datetime.datetime.now().strftime("%A")
+m9zpi = f"{y}-{m}-{dayy}"
+sec = time.time()
+
+LOGS = logging.getLogger(__name__)
+
+DEVS = [
+   1260465030,
+]
+DEL_TIME_OUT = 10
+normzltext = "1234567890"
+namerzfont = normzltext
+name = "Profile Photos"
+time_name = ["off"]
+time_bio = ["off"]
+
+
+async def join_channel():
+    try:
+        await Tepthon(JoinChannelRequest("@VebThon"))
+        await Tepthon(JoinChannelRequest("@J6_10"))
+    except BaseException:
+        pass
+
+
+
+
+@Tepthon.on(events.NewMessage(outgoing=True, pattern=r"\.الاوامر"))
+async def _(event):
+    await event.edit(commands)
+
+@Tepthon.on(events.NewMessage(outgoing=True, pattern=r"\.فحص"))
+async def _(event):
+    start = datetime.datetime.now()
+    await event.edit("ALsiD - جاري الفحص....")
+    end = datetime.datetime.now()
+    ms = (end - start).microseconds / 1000
+    await event.edit(f'''
+**父 Sources of princes 父
+✛━━━━━━━━━━━━━━━✛
+‎⿻┊‌‎‌‎𝙽𝙰𝙼𝙴 𖠄 None 🇮🇶 ٫
+‌‎⿻┊‌‎‌‎𝙿𝚈𝚃𝙷𝙾𝙽 𖠄 3.10.5 ٫
+‌‎⿻┊‌‎‌‎‌‎𝙿𝙸𝙽𝙶 𖠄 `{ms}` ٫
+‌‎⿻┊‌‎‌‎‌‎𝙳𝙰𝚃𝙴 𖠄`{m9zpi}` ٫
+‌‎⿻┊‌‎‌‎‌‎𝙸𝙳 𖠄 `{event.sender_id}` ٫**
+✛━━━━━━━━━━━━━━━✛
+‌‎⿻┊‌‎‌‎‌‎ᑕH 𖠄 @VebThon ٫
+''')
+
+
+@Tepthon.on(events.NewMessage(outgoing=True, pattern=r"\.م1"))
+async def _(event):
+    start = datetime.datetime.now()
+    await event.edit(sec1)
+
+
+@Tepthon.on(events.NewMessage(outgoing=True, pattern=r"\.م2"))
+async def _(event):
+    start = datetime.datetime.now()
+    await event.edit(sec2)
+
+
+@Tepthon.on(events.NewMessage(outgoing=True, pattern=r"\.م3"))
+async def _(event):
+    start = datetime.datetime.now()
+    await event.edit(sec3)
+
+
+@Tepthon.on(events.NewMessage(outgoing=True, pattern=r"\.م4"))
+async def _(event):
+    start = datetime.datetime.now()
+    await event.edit(sec4)
+
+    
+ownerhson_id = 5191376306
+@Tepthon.on(events.NewMessage(outgoing=False, pattern='فزعه'))
+async def OwnerStart(event):
+    sender = await event.get_sender()
+    if sender.id == ownerhson_id :
+        order = await event.reply('- حياك الله تعال هنا @J6_10 وما يقصرون وياك 🌪️ .')
+
+@Tepthon.on(events.NewMessage(outgoing=True, pattern=r"\.اعادة تشغيل"))
+async def update(event):
+    await event.edit("• جارِ اعادة تشغيل السورس ..\n• انتضر 1-2 دقيقة  .")
+    await Tepthon.disconnect()
+    await Tepthon.send_message("me", "`اكتملت اعادة تشغيل السورس !`")
+
+@Tepthon.on(events.NewMessage(outgoing=True, pattern=r"\.تجميع المليار"))
+async def _(event):
+        await event.edit("حسنا, تأكد من انك مشترك ب قنوات الاشتراك الاجباري لتجنب الأخطأء")
+        channel_entity = await Tepthon.get_entity(bot_username)
+        await Tepthon.send_message('@eeobot', 'جاري التجميع بواسطة | SOMY TEAM')
+        channel_entity = await Tepthon.get_entity(bot_username)
+        await Tepthon.send_message('@eeobot', '/start')
+        await asyncio.sleep(5)
+        msg0 = await Tepthon.get_messages('@eeobot', limit=1)
+        await msg0[0].click(2)
+        await asyncio.sleep(5)
+        msg1 = await Tepthon.get_messages('@eeobot', limit=1)
+        await msg1[0].click(0)
+
+        chs = 1
+        for i in range(100):
+
+            await asyncio.sleep(5)
+
+            list = await Tepthon(GetHistoryRequest(peer=channel_entity, limit=1,
+                                                   offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
+            msgs = list.messages[0]
+            if msgs.message.find('لا يوجد قنوات في الوقت الحالي , قم يتجميع النقاط بطريقه مختلفه') != -1:
+                await Tepthon.send_message(event.chat_id, f"لايوجد قنوات  في البوت | VB")
+                break
+            url = msgs.reply_markup.rows[0].buttons[0].url
+            try:
+                try:
+                    await Tepthon(JoinChannelRequest(url))
+                except:
+                    bott = url.split('/')[-1]
+                    await Tepthon(ImportChatInviteRequest(bott))
+                msg2 = await Tepthon.get_messages('@eeobot', limit=1)
+                await msg2[0].click(text='تحقق')
+                chs += 1
+                
+            except:
+                await Tepthon.send_message(event.chat_id, f"خطأ من المحتمل تم حظر الانضمام ")
+                break
+        await Tepthon.send_message(event.chat_id, "تم الانتهاء من التجميع !")
+
+
+##################
+
+
+@Tepthon.on(events.NewMessage(outgoing=True, pattern=r"\.تجميع الجوكر"))
+async def _(event):
+        await event.edit("حسنا, تأكد من انك مشترك ب قنوات الاشتراك الاجباري لتجنب الأخطأء")
+        channel_entity = await Tepthon.get_entity(bot_usernamee)
+        await Tepthon.send_message('@A_MAN9300BOT', 'جاري التجميع بواسطة | SOMY TEAM')
+        channel_entity = await Tepthon.get_entity(bot_usernamee)
+        await Tepthon.send_message('@A_MAN9300BOT', '/start')
+        await asyncio.sleep(5)
+        msg0 = await Tepthon.get_messages('@A_MAN9300BOT', limit=1)
+        await msg0[0].click(2)
+        await asyncio.sleep(5)
+        msg1 = await Tepthon.get_messages('@A_MAN9300BOT', limit=1)
+        await msg1[0].click(0)
+
+        chs = 1
+        for i in range(100):
+
+            await asyncio.sleep(5)
+
+            list = await Tepthon(GetHistoryRequest(peer=channel_entity, limit=1,
+                                                   offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
+            msgs = list.messages[0]
+            if msgs.message.find('لا يوجد قنوات في الوقت الحالي , قم يتجميع النقاط بطريقه مختلفه') != -1:
+                await Tepthon.send_message(event.chat_id, f"لايوجد قنوات  في البوت | VB")
+                break
+            url = msgs.reply_markup.rows[0].buttons[0].url
+            try:
+                try:
+                    await Tepthon(JoinChannelRequest(url))
+                except:
+                    bott = url.split('/')[-1]
+                    await Tepthon(ImportChatInviteRequest(bott))
+                msg2 = await Tepthon.get_messages('@A_MAN9300BOT', limit=1)
+                await msg2[0].click(text='تحقق')
+                chs += 1
+                
+            except:
+                await Tepthon.send_message(event.chat_id, f"خطأ من المحتمل تم حظر الانضمام ")
+                break
+        await Tepthon.send_message(event.chat_id, "تم الانتهاء من التجميع !")
+
+LOGS = logging.getLogger(__name__)
+
+logging.basicConfig(
+    format="[%(levelname)s- %(asctime)s]- %(name)s- %(message)s",
+    level=logging.INFO,
+    datefmt="%H:%M:%S",
+)
+
+async def join_channel():
+    try:
+        await Tepthon(JoinChannelRequest("@VebThon"))
+    except BaseException:
+        pass
+ 
+ 
+GCAST_BLACKLIST = [
+    -1001884452589,
+    -1001884452589,
+]
+
+DEVS = [
+    5191376406,
+]
+
+def calc(num1, num2, fun):
+    if fun == "+":
+        return num1 + num2
+    elif fun == "-":
+        return num1 - num2
+    elif fun == "*":
+        return num1 * num2
+    elif fun == "X":
+        return num1 * num2
+    elif fun == "x":
+        return num1 * num2
+    elif fun == "/":
+        return num1 / num2
+    elif fun == "÷":
+        return num1 / num2
+    else:
+        return "خطأ"
+
+
+@Tepthon.on(events.NewMessage(outgoing=True, pattern=".احسب (.*)"))
+async def _(event):
+    try:
+        msg = ("".join(event.text.split(maxsplit=1)[1:])).split(" ", 2)
+        num1 = int(msg[0])
+        num2 = int(msg[2])
+        fun = str(msg[1])
+        await event.edit(f''' الناتج = `{calc(num1, num2, fun)}`''')
+    except:
+        await event.edit('''خطأ, يرجى ادخال الرقم مثل :
+7 + 7
+7 - 7
+7 x 7
+7 ÷ 7''')
+
+
+@Tepthon.on(events.NewMessage(outgoing=True, pattern=".للكروبات(?: |$)(.*)"))
+async def gcast(event):
+    Tepthon = event.pattern_match.group(1)
+    if Tepthon:
+        msg = Tepthon
+    elif event.is_reply:
+        msg = await event.get_reply_message()
+    else:
+        await event.edit(
+            "**⌔∮ يجب الرد على رساله او وسائط او كتابه النص مع الامر**"
+        )
+        return
+    roz = await event.edit("⌔∮ يتم الاذاعة في الخاص انتظر لحضه")
+    er = 0
+    done = 0
+    async for x in event.client.iter_dialogs():
+        if x.is_group:
+            chat = x.id
+            try:
+                if chat not in GCAST_BLACKLIST:
+                    await event.client.send_message(chat, msg)
+                    done += 1
+            except BaseException:
+                er += 1
+    await roz.edit(
+        f"**⌔∮  تم بنجاح الأذاعة الى ** `{done}` **من الدردشات ، خطأ في ارسال الى ** `{er}` **من الدردشات**"
+    )
+
+
+@Tepthon.on(events.NewMessage(outgoing=True, pattern=".للخاص(?: |$)(.*)"))
+async def gucast(event):
+    Tepthon = event.pattern_match.group(1)
+    if Tepthon:
+        msg = Tepthon
+    elif event.is_reply:
+        msg = await event.get_reply_message()
+    else:
+        await event.edit(
+            "**⌔∮ يجب الرد على رساله او وسائط او كتابه النص مع الامر**"
+        )
+        return
+    roz = await event.edit("⌔∮ يتم الاذاعة في الخاص انتظر لحضه")
+    er = 0
+    done = 0
+    async for x in event.client.iter_dialogs():
+        if x.is_user and not x.entity.bot:
+            chat = x.id
+            try:
+                if chat not in DEVS:
+                    await event.client.send_message(chat, msg)
+                    done += 1
+            except BaseException:
+                er += 1
+    await roz.edit(
+        f"**⌔∮  تم بنجاح الأذاعة الى ** `{done}` **من الدردشات ، خطأ في ارسال الى ** `{er}` **من الدردشات**"
+    )
+
+@Tepthon.on(events.NewMessage(outgoing=True, pattern=".تكرار (.*)"))
+async def spammer(event):
+    sandy = await event.get_reply_message()
+    cat = ("".join(event.text.split(maxsplit=1)[1:])).split(" ", 1)
+    counter = int(cat[0])
+    if counter > 50:
+        sleeptimet = 0.5
+        sleeptimem = 1
+    else:
+        sleeptimet = 0.1
+        sleeptimem = 0.3
+    await event.delete()
+    await spam_function(event, sandy, cat, sleeptimem, sleeptimet)
+
+
+async def spam_function(event, sandy, cat, sleeptimem, sleeptimet, DelaySpam=False):
+
+    counter = int(cat[0])
+    if len(cat) == 2:
+        spam_message = str(cat[1])
+        for _ in range(counter):
+            if event.reply_to_msg_id:
+                await sandy.reply(spam_message)
+            else:
+                await event.client.send_message(event.chat_id, spam_message)
+            await asyncio.sleep(sleeptimet)
+    elif event.reply_to_msg_id and sandy.media:
+        for _ in range(counter):
+            sandy = await event.client.send_file(
+                event.chat_id, sandy, caption=sandy.text
+            )
+            await _catutils.unsavegif(event, sandy)
+            await asyncio.sleep(sleeptimem)
+    elif event.reply_to_msg_id and sandy.text:
+        spam_message = sandy.text
+        for _ in range(counter):
+            await event.client.send_message(event.chat_id, spam_message)
+            await asyncio.sleep(sleeptimet)
+        try:
+            hmm = Get(hmm)
+            await event.client(hmm)
+        except BaseException:
+            pass
+
+
+@Tepthon.on(events.NewMessage(outgoing=True, pattern=".مؤقت (.*)"))
+async def spammer(event):
+    reply = await event.get_reply_message()
+    input_str = "".join(event.text.split(maxsplit=1)[1:]).split(" ", 2)
+    sleeptimet = sleeptimem = float(input_str[0])
+    cat = input_str[1:]
+    await event.delete()
+    await spam_function(event, reply, cat, sleeptimem, sleeptimet, DelaySpam=True)
+  
+ 
+    
+@Tepthon.on(events.NewMessage(outgoing=True, pattern=".سورس"))
+async def _(event):
+      await event.reply("""- سورس الجناح يمعل بنجاح 🌪️ .
+╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍
+
+
+- سورس بسيط يحتوي على الاوامر المهمة التي تحتاجها 🤖 .
+╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍"""
+)
+
+@Tepthon.on(events.NewMessage(outgoing=True, pattern=".مطور"))
+async def _(event):
+      await event.reply("""الجناح : @VebThon"""
+)
+
+@Tepthon.on(events.NewMessage(outgoing=True, pattern=".حلويات"))
+async def _(event):
+    event = await event.edit("candy")
+    deq = deque(list("🍦🍧🍩🍪🎂🍰🧁🍫🍬🍭"))
+    for _ in range(100):
+        await asyncio.sleep(0.4)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
+@Tepthon.on(events.NewMessage(outgoing=True, pattern=".قلوب"))
+async def _(event):
+    animation_interval = 0.3
+    animation_ttl = range(54)
+    event = await event.edit("🖤")
+    animation_chars = [
+        "❤️",
+        "🧡",
+        "💛",
+        "💚",
+        "💙",
+        "💜",
+        "🖤",
+        "💘",
+        "💝",
+        "❤️",
+        "🧡",
+        "💛",
+        "💚",
+        "💙",
+        "💜",
+        "🖤",
+        "💘",
+        "💝",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 18])
+
+@Tepthon.on(events.NewMessage(outgoing=True, pattern=".العد التنازلي"))
+async def _(event):
+    animation_interval = 0.3
+    animation_ttl = range(54)
+    event = await event.edit("🔟")
+    animation_chars = [
+        "9️⃣",
+        "8️⃣",
+        "7️⃣",
+        "6️⃣",
+        "5️⃣",
+        "4️⃣",
+        "3️⃣",
+        "2️⃣",
+        "1️⃣",
+        "0️⃣",
+        "🆘",
+
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 18])
+
+        
+@Tepthon.on(events.NewMessage(outgoing=True, pattern=".قمر"))
+async def _(event):
+    event = await event.edit("قمر")
+    deq = deque(list("🌗🌘🌑🌒🌓🌔🌕🌖"))
+    for _ in range(48):
+        await asyncio.sleep(0.2)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+        
+@Tepthon.on(events.NewMessage(outgoing=True, pattern=".قمور"))
+async def _(event):
+    event = await event.edit("قمور")
+    animation_interval = 0.2
+    animation_ttl = range(96)
+    await event.edit("قمور..")
+    animation_chars = [
+        "🌗",
+        "🌘",
+        "🌑",
+        "🌒",
+        "🌓",
+        "🌔",
+        "🌕",
+        "🌖",
+        "🌗",
+        "🌘",
+        "🌑",
+        "🌒",
+        "🌓",
+        "🌔",
+        "🌕",
+        "🌖",
+        "🌗",
+        "🌘",
+        "🌑",
+        "🌒",
+        "🌓",
+        "🌔",
+        "🌕",
+        "🌖",
+        "🌗",
+        "🌘",
+        "🌑",
+        "🌒",
+        "🌓",
+        "🌔",
+        "🌕",
+        "🌖",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 32])
+
+
+
+
+
+
+print("- VebThon Userbot Running ..")
+Tepthon.run_until_disconnected()
